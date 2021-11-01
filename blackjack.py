7ae918b556a3bc1c5f08f566a1bed7e56ce3afe5
memcmahon <mcmahon.meganelizabeth@gmail.com>
2021-11-01 16:32:18 -0600

    blackjack wip

---
+++ blackjack.py
@@ -0,0 +1,138 @@
+import random, sys
+
+# Set up the constants:
+HEARTS = chr(9829)
+DIAMONDS = chr(9830)
+SPADES = chr(9824)
+CLUBS = chr(9827)
+BACKSIDE = 'backside'
+
+def main():
+    print('''Blackjack
+
+    Rules:
+        Try to get as close to 21 without going over.
+        Kings, Queens, and Jacks are worth 10 points.
+        Aces are worth 1 or 11 points.
+        Cards 2 through 10 are worth their face value.
+        (H)it to take another card.
+        (S)tand to stop taking cards.
+        On your first play, you can (D)double down to increase your bet,
+        but must hit exactly one more time before standing.
+        In case of a tie, the bet is returned to the player.
+        The dealer stops hitting at 17.''')
+
+    money = 5000
+    while True: #main game loop
+        if money <= 0:
+            print("You're broke!")
+            sys.exit()
+
+        print('Money:', money)
+        bet = getBet(money)
+
+        # Deal 2 cards to the player and dealer
+        deck = getDeck()
+        dealerHand = [deck.pop(), deck.pop()]
+        playerHand = [deck.pop(), deck.pop()]
+
+        # Player actions:
+        print('Bet:', bet)
+
+        # while True:
+        displayHands(playerHand, dealerHand, False)
+        print()
+
+        # check if player has busted:
+        if getHandValue(playerHand) > 21:
+            break
+
+        # get the players move - H, S, or D:
+        move = getMove(playerHand, money - bet)
+
+
+def getBet(maxBet):
+    """Ask the player how much money they want to bet for this round."""
+    while True:
+        print('How much do you want to bet? (1-{}, or QUIT)'.format(maxBet))
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+        if not bet.isdecimal():
+            # continue #ask again
+            sys.exit()
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet
+
+def getDeck():
+    """Return a list of (rank, suit) tuples for all 52 cards"""
+    deck = []
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2,11):
+            deck.append((str(rank), suit))
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit))
+    random.shuffle(deck)
+    return deck
+
+def displayHands(playerHand, dealerHand, showDealerHand):
+    """Show the player's and dealer's cards. Hide the dealer's first
+    card if showDealerHand is False."""
+    if showDealerHand:
+        print('Dealer:', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER: ???')
+        displayCards([BACKSIDE] + dealerHand[1:])
+
+    print('PLAYER:', getHandValue(playerHand))
+    displayCards(playerHand)
+
+def getHandValue(cards):
+    """Returns the value of the cards.  Ace is worth 11 or 1.  This function
+    chooses the most appropriate value for Ace."""
+    value = 0
+    numberOfAces = 0
+
+    # add value for non-Ace cards
+    for card in cards:
+        rank = card[0]
+        if rank == 'A':
+            numberOfAces += 1
+        elif rank.isdecimal():
+            value += int(rank)
+        else:
+            value += 10
+
+    # add value for Aces
+    value += numberOfAces
+    for i in range(numberOfAces):
+        if value + 10 <= 21:
+            value += 10
+
+    return value
+
+def displayCards(cards):
+    """Display all the cards in the hand"""
+    rows = ['', '', '', '', '']
+    for i, card in enumerate(cards):
+        rows[0] += ' ___  '
+        if card == BACKSIDE:
+            rows[1] += '|## |'
+            rows[2] += '|###|'
+            rows[3] += '|_##|'
+        else:
+            rank, suit = card
+            rows[1] += '|{} | '.format(rank.ljust(2))
+            rows[2] += '| {} | '.format(suit)
+            rows[3] += '|_{}| '.format(rank.rjust(2, '_'))
+
+    for row in rows:
+        print(row)
+
+
+
+if __name__ == '__main__':
+    main()
