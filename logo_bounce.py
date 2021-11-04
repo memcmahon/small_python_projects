e03bf28276c6505360b9746ced0a60039821e117
memcmahon <mcmahon.meganelizabeth@gmail.com>
2021-11-04 12:28:46 -0600

    logo bounce

--- logo_bounce.py
+++ logo_bounce.py
@@ -3,7 +3,7 @@
 import sys, random, time, bext
 
 WIDTH, HEIGHT   = bext.size()
-NUMBER_OF_LOGOS = 5
+NUMBER_OF_LOGOS = 10
 PAUSE_AMOUNT    = 0.2
 COLORS          = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
@@ -23,11 +23,11 @@ def main():
     bext.clear()
 
     logos = []
-    for i in rance(NUMBER_OF_LOGOS):
+    for i in range(NUMBER_OF_LOGOS):
         logos.append(
             {COLOR: random.choice(COLORS),
              X: random.randint(1, WIDTH - 4),
-             Y: random.rand_int(1, HEIGHT - 4),
+             Y: random.randint(1, HEIGHT - 4),
              DIR: random.choice(DIRECTIONS)
             }
         )
@@ -35,8 +35,81 @@ def main():
             logos[-1][X] -= 1
 
     cornerBounces = 0
-    # while TRUE:
+    while True:
+        for logo in logos:
+            bext.goto(logo[X], logo[Y])
+            print('   ', end='')
+
+            originalDirection = logo[DIR]
+
+            if logo[X] == 0 and logo[Y] == 0:
+                logo[DIR] = DOWN_RIGHT
+                cornerBounces += 1
+            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_RIGHT
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == 0:
+                logo[DIR] = DOWN_LEFT
+                cornerBounces += 1
+            elif logo[X] == WIDTH -3 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_LEFT
+                cornerBounces += 1
+
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = UP_RIGHT
+            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = DOWN_RIGHT
+
+            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT
+            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT
+
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = DOWN_LEFT
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = DOWN_RIGHT
+
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = UP_LEFT
+            elif logo[Y] == HEIGHT - 1 and logo [DIR] == DOWN_RIGHT:
+                logo[DIR] = UP_RIGHT
+
+            if logo[DIR] != originalDirection:
+                logo[COLOR] = random.choice(COLORS)
+
+            if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+            elif logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+            elif logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+            elif logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
+
+        bext.goto(5, 0)
+        bext.fg('white')
+        print('Corner bounces:', cornerBounces, end='')
+
+        for logo in logos:
+            bext.goto(logo[X], logo[Y])
+            bext.fg(logo[COLOR])
+            print('DVD', end='')
+
+        bext.goto(0,0)
+
+        sys.stdout.flush()
+        time.sleep(PAUSE_AMOUNT)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print('Bouncing Logos')
+        sys.exit()
